a0ec3f1926593a8229405e97adfc39ca61f8cc38
Phoenix <lebogang.mosopa@outlook.com>
2025-07-07 21:16:38 +0200

    Created dockerfile

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import django
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Like_a_phoenix'
+copyright = '2025, PHOENIX'
+author = 'PHOENIX'
+release = '2025/07/01'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+
+sys.path.insert(0, os.path.abspath('../../'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Like_a_Phoenix.settings'
+django.setup()
