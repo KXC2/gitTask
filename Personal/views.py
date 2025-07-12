9e2aa5ea20a4a9bfc8355848bafc4b210307d5df
Phoenix <lebogang.mosopa@outlook.com>
2025-07-12 16:01:28 +0200

    Adding docstrings to a few methods/classes\

--- Personal/views.py
+++ Personal/views.py
@@ -10,17 +10,21 @@ from .models import Puns, Thoughts, DiaryEntry
 
 def user_login(request):
 
-    '''This function is intended for when the user logs in, they are 
-    taken directly to index.html file without requesting any input.'''
+    """
+    This function is intended for when the user logs in, they are 
+    taken directly to index.html file without requesting any input.
+    """
 
     return render(request, 'index.html')
 
 def authenticate_user(request):
 
-    '''The user is authenticated. The username and password are requested,
+    """
+    The user is authenticated. The username and password are requested,
     from the previous file and if successfully authenticated, the
     system will proceed to home.html, if not the system will loop back
-    to the current page'''
+    to the current page
+    """
 
     username = request.POST['username']
     password = request.POST['password']
@@ -35,9 +39,11 @@ def authenticate_user(request):
 
 def account_creation(request):
 
-    '''When prompted the system will fetch PhoenixForm and will required,
+    """
+    When prompted the system will fetch PhoenixForm and will required,
     to enter their details before proceeding, else the system will loop
-    to the the PhoenixForm and repeat process'''
+    to the the PhoenixForm and repeat process
+    """
 
     if request.method == 'POST':
         form = PhoenixForms(request.POST)
@@ -51,9 +57,11 @@ def account_creation(request):
 
 def phoenixDiary(request):
 
-    '''Diary entries are fetched and sorted by the latest entry, thus
+    """
+    Diary entries are fetched and sorted by the latest entry, thus
     taking the final results to the PhoenixDiary.html. In a case where
-    none entries where found, the appropriate message will be displayed'''
+    none entries where found, the appropriate message will be displayed
+    """
 
     Latest_diary = DiaryEntry.objects.order_by('-pub_date')[:5]
     return render(request, "PhoenixDiary.html", {
@@ -62,9 +70,11 @@ def phoenixDiary(request):
 
 def jokes(request):
 
-    '''Jokes contents are fetched and sorted by the latest content, thus
+    """
+    Jokes contents are fetched and sorted by the latest content, thus
     taking the final results to the puns.html. In a case where
-    none contents where found, the appropriate message will be displayed'''
+    none contents where found, the appropriate message will be displayed
+    """
 
     Latest_pun = Puns.objects.all()
     return render(request, "puns.html", {
@@ -73,9 +83,11 @@ def jokes(request):
 
 def thoughts(request):
 
-    '''Thoughts contents are fetched and sorted by the latest content, thus
+    """
+    Thoughts contents are fetched and sorted by the latest content, thus
     taking the final results to the thoughts.html. In a case where
-    none contents where found, the appropriate message will be displayed'''
+    none contents where found, the appropriate message will be displayed
+    """
 
     Latest_thoughts = Thoughts.objects.order_by('-pub_date')[:5]
     return render(request, "thoughts.html", {
---
+++ README.md
@@ -0,0 +1,59 @@
+
+# Like a Phoenix
+Welcome to *Like_a_Phoenix*, experience the real feeling of being a phoenix. This Django-webapp is designed to showcase the feeling of that of a Phoenix, 
+with exposure to
+
+	1. Diary entries, the daily life of the Phoenix
+ 
+	2. Programming Puns, jokes that are meant for coding nerds
+ 
+	3. Weird thoughts, the circling thoughts of Phoenix
+
+A weird and perfect blend of Chaos, creativity and code- like a Phoenix.
+
+
+## System requirements:
+
+Make sure you have:
+
+- Python 3.8+
+- pip
+- virtualenv (recommended)
+
+## Installation
+In your terminal run:
+
+	1. git clone https://github.com/KXC2/Django-gitTask
+	2. cd Like_a_Phoenix
+	3. python -m venv venv
+
+### For windows OS
+
+	4. venv\Scripts\activate
+
+### For Mac/Linux OS
+
+	4. source venv/bin/activate
+
+### Continuation
+
+	5. pip install -r requirements.txt
+	6. python manage.py migrate
+	7. python manage.py runserver
+
+
+### For Dockerfile usage
+
+	1. docker build -t like_a_phoenix .
+	2. docker run -p 8000:8000 like_a_phoenix
+
+
+## Acknowledgements
+ - https://www.readme-templates.com/
+ - https://readme.so/
+g
+Note: Some jokes and puns were outsourced on:
+
+	- https://www.databasestar.com/programmer-jokes/
+	- https://www.reddit.com/r/Showerthoughts/
+
